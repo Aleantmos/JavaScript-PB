1cebd7a6b7b35e2fcbd6bbf835e2a6e277482518
Aleantmos <112842147+Aleantmos@users.noreply.github.com>
2022-12-08 15:21:07 +0200

    Delete FirstStepsEx directory

--- FirstStepsEx/1.Converter.js
+++
@@ -1,7 +0,0 @@
-function converter(input) {
-    let exchangeRatio = 1.79549;
-    let number = input[0];
-    let numInLev =  number * exchangeRatio;
-
-    console.log(`${numInLev}`);
-}
--- FirstStepsEx/2.RadiansToDegrees.js
+++
@@ -1,5 +0,0 @@
-function ConverterGrad(input) {
-    let radian = input[0];
-    let grad = radian * 180/Math.PI;
-    console.log (`${grad}`);
-}
